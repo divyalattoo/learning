39633c702d1fbc393be27aef954ca0e8d8b7f929
Divya <divya@Divyas-MacBook-Air.local>
2025-05-26 14:18:24 +0530

    Admin pannel added, updated

--- tryhello/admin.py
+++ tryhello/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from tryhello.models import Students, Course, StudentCourse
+
 # Register your models here.
+# admin.site.register(Students)
+admin.site.register(Course)
+admin.site.register(StudentCourse)
+
+# Modifying Student class and customizing the panel for editing and adding student
+class StudentAdmin(admin.ModelAdmin):
+    list_filter = (('name'),) #Tuple
+    list_display = ('id', 'name', 'age')
+    # list_display_links = (('name'),)
+    list_editable = (('name'),)
+    search_fields = ('name',)
+admin.site.register(Students, StudentAdmin)
--- tryhello/views.py
+++ tryhello/views.py
@@ -1,6 +1,18 @@
 from django.http import HttpResponse
-
+from tryhello.models import Students
+import datetime
 
 def hello_world(request,name):
-    return HttpResponse("Hello " + name)
+    # adding in DB
+    # s1 = Students(name = name, age = 10, dob = datetime.date.today())
+    # s1.save() # to save in DB
+
+    # getting data of all student
+    all_students = Students.objects.all()
+
+    #geeting specific student and updating a column
+    student_1 = Students.objects.get(name=name)
+    student_1.age = 15
+    student_1.save()
+    return HttpResponse("Hello, your details are " + student_1.name + " updated age " + str(student_1.age) + " dob " + str(student_1.dob))
 
